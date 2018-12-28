715856a2ca2cd4ed2d0f0b1b8787da35182eeddb
Jake Worth <jake@jakeworth.com>
2018-12-28 14:25:50 -0600

    Add Gatsby plugin manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,11 +19,25 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
+        name: 'openingday-progressbar',
+        short_name: 'openingday-progressbar',
+        start_url: '/',
+        background_color: '#6b37bf',
+        theme_color: '#6b37bf',
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: 'standalone',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#002D70`,
+        theme_color: `#002D70`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
