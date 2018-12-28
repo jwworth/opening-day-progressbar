49beb726eb317c63c70bc9910bbead50a5fffe02
Jake Worth <jake@jakeworth.com>
2018-12-28 14:25:49 -0600

    Add basic metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Opening Day Progress Bar`,
+    description: `A bar showing progress toward MLB Opening Day`,
+    author: `@jwworth`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
